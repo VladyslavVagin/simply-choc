d877c2c4fdc09c2ec07c5a04a6d03ed418a80ac0
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-22 23:12:52 +0200

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -15,10 +15,11 @@ function chooseProduct () {
 function addToCart(event) {
    const choosenProduct = event.target.getAttribute('value');
    for (let product of products) {
-     if (products.indexOf(product) === choosenProduct - 1) {
+     if (products.indexOf(product) === choosenProduct - 1 && !choosenProductsForCart.includes(product)) {
         choosenProductsForCart.push(product);
         localStorage.setItem('Product', JSON.stringify(choosenProductsForCart));
-        console.log(localStorage);
+        console.log(product.id);
+        console.log(choosenProductsForCart);
      }
    }
 };
