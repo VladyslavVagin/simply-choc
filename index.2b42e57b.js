2bac92e19aaefb480fa76d2d57d7f617c165da1e
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-22 13:02:16 +0000

    Deploying to gh-pages from @ VladyslavVagin/simply-choc@1fd6c1e16c52c3ea3705a7e8ad6568d6b8c4d4d2 🚀

---
+++ index.2b42e57b.js
@@ -0,0 +1,2 @@
+jQuery(document).ready((function(e){var o,a;o=e(".f-video-cover, .play-icon"),a=e(".f-video-player iframe"),o.on("click",(function(){o.fadeOut(),a[0].src+="&autoplay=1"}))}));
+//# sourceMappingURL=index.2b42e57b.js.map
---
+++ index.7880434d.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]"),body:document.querySelector("body")};function o(){e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("backdrop-burger")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.7880434d.js.map
