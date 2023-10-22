bc598dd01ed249c70f2dc5e030fd6d1d025e117c
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-22 19:23:50 +0200

    add list products

---
+++ src/js/products.js
@@ -0,0 +1,41 @@
+export const products = [orange = {
+    id: 1,
+    name: 'orange',
+    type: 'dark chocolate',
+    price: 45
+}, apple = {
+    id: 2,
+    name: 'apple & cranberry',
+    type: 'milk chocolate',
+    price: 50
+}, lime = {
+    id: 3,
+    name: 'lime & sea salt',
+    type: 'dark chocolate',
+    price: 66
+}, pineapple = {
+    id: 4,
+    name: 'pineapple',
+    type: 'dark chocolate',
+    price: 54
+}, classic = {
+    id: 5,
+    name: 'classic',
+    type: 'milk chocolate',
+    price: 45
+}, honey = {
+    id: 6,
+    name: 'honey',
+    type: 'milk chocolate',
+    price: 50
+}, roasted = {
+    id: 7,
+    name: 'roasted fruits',
+    type: 'dark chocolate',
+    price: 66
+}, white = {
+    id: 8,
+    name: 'classic',
+    type: 'white chocolate',
+    price: 54
+}];
