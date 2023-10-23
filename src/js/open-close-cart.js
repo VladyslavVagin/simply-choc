d396733cd33337bf2488749c04ca9edba769f7a6
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-23 07:47:41 +0200

    update

--- src/js/open-close-cart.js
+++ src/js/open-close-cart.js
@@ -2,14 +2,14 @@
     const refs = {
       openCartBtn: document.querySelector('[data-cart-open]'),
       closeCartBtn: document.querySelector('[data-cart-close]'),
-      cartData: document.querySelector('[data-cart]')
+      cartData: document.querySelector('[data-cart]'),
     };
   
     refs.openCartBtn.addEventListener('click', cartAction);
     refs.closeCartBtn.addEventListener('click', cartAction);
   
      function cartAction() {
-      refs.cartData.classList.toggle('is-hidden');
+      refs.cartData.classList.toggle('visual');
      };
     };
 
