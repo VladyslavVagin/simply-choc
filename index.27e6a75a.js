f107c7d8baf160cbf619b30a60656022dfd9836a
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-23 05:48:17 +0000

    Deploying to gh-pages from @ VladyslavVagin/simply-choc@d396733cd33337bf2488749c04ca9edba769f7a6 🚀

---
+++ index.27e6a75a.js
@@ -0,0 +1,2 @@
+function cartOpenClose(){var t={openCartBtn:document.querySelector("[data-cart-open]"),closeCartBtn:document.querySelector("[data-cart-close]"),cartData:document.querySelector("[data-cart]")};function e(){t.cartData.classList.toggle("visual")}t.openCartBtn.addEventListener("click",e),t.closeCartBtn.addEventListener("click",e)}cartOpenClose();
+//# sourceMappingURL=index.27e6a75a.js.map
--- index.bbc0a181.js
+++
@@ -1,2 +0,0 @@
-function cartOpenClose(){var t={openCartBtn:document.querySelector("[data-cart-open]"),closeCartBtn:document.querySelector("[data-cart-close]"),cartData:document.querySelector("[data-cart]")};function e(){t.cartData.classList.toggle("is-hidden")}t.openCartBtn.addEventListener("click",e),t.closeCartBtn.addEventListener("click",e)}cartOpenClose();
-//# sourceMappingURL=index.bbc0a181.js.map
