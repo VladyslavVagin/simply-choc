a16f329513b8224b1f542f20871b6f539043f1ab
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-23 21:11:23 +0200

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,8 +5,6 @@ const listOfProds = document.querySelector('.products-cards');
 const totalCost = document.querySelector('.total-dinero');
 console.log(totalCost.textContent);
 let choosenProductsForCart = [];
-let result = 0;
-totalCost.textContent = result;
 
  addProducts.forEach(addButton => addButton.addEventListener('click', addToCart));
 
@@ -14,25 +12,22 @@ function addToCart(event) {
    const choosenProduct = event.currentTarget.getAttribute('value');
    for ( let product of products ) {
     if (products.indexOf(product) === choosenProduct - 1 && !choosenProductsForCart.includes(product)) {
-    choosenProductsForCart.push(product); 
-    localStorage.setItem('Prod', JSON.stringify(choosenProductsForCart));  
     console.log(product);
-
-}
+    listOfProds.insertAdjacentHTML('beforeend', markupProductCard(product));
+    choosenProductsForCart.push(product); 
+    localStorage.setItem('Prod', JSON.stringify(choosenProductsForCart));
+    } 
    }
 };
 
 
-
-
-
 // ==================================================MARKUP CARD ====================================
 function markupProductCard(product) {
-    const markupP = `<li class="prod-card">
+   return  markupP = `<li class="prod-card">
     <h4 class="prod-card-title">${product.type} "${product.name}"</h4>
     <div id="counter">
       <button type="button" data-action="decrement">-1</button>
-      <span id="value">${result}</span>
+      <span id="value">1</span>
       <button type="button" data-action="increment">+1</button>
     </div>
     <h5>Price: <span>${product.price} UAH</span></h5>
