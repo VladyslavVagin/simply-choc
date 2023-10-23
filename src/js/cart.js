17b5fe4872745cd94fb193c0008bb2f20d8b333f
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-23 18:52:21 +0200

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,59 +1,41 @@
 import { products } from "./products";
-console.log(products);
 
 const addProducts = document.querySelectorAll('.add-product-button');
-const listOfProducts = document.querySelector('.products-cards');
+const listOfProds = document.querySelector('.products-cards');
+const totalCost = document.querySelector('.total-dinero');
+console.log(totalCost.textContent);
 let choosenProductsForCart = [];
-
-// ======================== COUNTER VALUE =======================================
-const counterValue = document.querySelector('#value');
 let result = 0;
+totalCost.textContent = result;
 
-// const btnIncrement = document.querySelector('[data-action="increment"]');
-// const btnDecrement = document.querySelector('[data-action="decrement"]');
-
-// btnIncrement.addEventListener('click', () => counterValue.textContent = result += 1);
-// btnDecrement.addEventListener('click', () => counterValue.textContent = result -= 1);
-// =================================================================================
-chooseProduct();
-
-function chooseProduct () {
-    addProducts.forEach(addProdButton => {
-        addProdButton.addEventListener('click', addToCart);
-    })
-};
+ addProducts.forEach(addButton => addButton.addEventListener('click', addToCart));
 
 function addToCart(event) {
-   const choosenProduct = event.target.getAttribute('value');
-   for (let product of products) {
-     if (products.indexOf(product) === choosenProduct - 1 && !choosenProductsForCart.includes(product)) {
-        choosenProductsForCart.push(product);
-        listOfProducts.insertAdjacentHTML('beforeend', markupProductCard(product));
+   const choosenProduct = event.currentTarget.getAttribute('value');
+   for ( let product of products ) {
+    if (products.indexOf(product) === choosenProduct - 1 && !choosenProductsForCart.includes(product)) {
+    choosenProductsForCart.push(product); 
+    localStorage.setItem('Prod', JSON.stringify(choosenProductsForCart));  
+    console.log(product);
 
-        // DELETE BUTTON 
-        const deleteProductButtons = document.querySelector('.delete-product');
-        console.log(deleteProductButtons);
-
-        localStorage.setItem('Product', JSON.stringify(choosenProductsForCart));
-        console.log(product.id);
-        console.log(choosenProductsForCart);
-     }
+}
    }
 };
 
 
-//================================== MARKUP PRODUCT CARDS=================================== 
-function markupProductCard (product) {
-  return templateOfCard = `<li class="prod-card">
-  <h4><span>${product.type}</span>"${product.name}"</h4>
-  <div id="counter">
-    <button type="button" data-action="decrement">-1</button>
-    <span id="value">${result}</span>
-    <button type="button" data-action="increment">+1</button>
-  </div>
-  <h5>Price: <span>${product.price}</span></h5>
-  <button type="button" class="delete-product">Delete</button>
-</li>`;
-};
 
-// =========================================================================================
+
+
+// ==================================================MARKUP CARD ====================================
+function markupProductCard(product) {
+    const markupP = `<li class="prod-card">
+    <h4 class="prod-card-title">${product.type} "${product.name}"</h4>
+    <div id="counter">
+      <button type="button" data-action="decrement">-1</button>
+      <span id="value">${result}</span>
+      <button type="button" data-action="increment">+1</button>
+    </div>
+    <h5>Price: <span>${product.price} UAH</span></h5>
+    <button type="button" class="delete-product" value="${product.id}">Delete</button>
+  </li>`;
+};
