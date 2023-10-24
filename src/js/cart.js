c90eda70a011e4c2feb84165af2088766681e20c
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-24 14:41:52 +0200

    fhjfgjhgf

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,3 @@
 import products from './products';
 console.log(products);
 
-const addProduct = document.querySelectorAll('.add-product-button');
-
-addProduct.forEach(addBtn => addBtn.addEventListener('click', addProductToCart));
-
-function addProductToCart(event) {
-    event.preventDefault();
-};
-
-
