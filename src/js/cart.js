c4a2748ab9447b60ced876775d11d7f0e255d059
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-24 10:19:07 +0200

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,56 +1,15 @@
-import { products } from "./products";
+const cardOfProduct = document.querySelectorAll('.prod-card');
+const addToBtn = document.querySelectorAll('.add-product-button');
+const counter = document.querySelectorAll('#value');
 
-//=================================================== ALL CONSTANT ==============================
-const addProducts = document.querySelectorAll('.add-product-button');
-const listOfProds = document.querySelector('.products-cards');
-const totalCost = document.querySelector('.total-dinero');
-const noProd = document.querySelector('.no-prod');
-// =================================================================---------===================
-let choosenProductsForCart = [];
+cardOfProduct.forEach(card => card.classList.add('visually-hidden'));
+addToBtn.forEach(addBtn => addBtn.addEventListener('click', addCard));
 
-//================================== FOR EACH ADD BUTTON PUT EVENT LISTENER =========================
- addProducts.forEach(addButton => addButton.addEventListener('click', addToCart));
-
-// ======================================== FUNCTION FOR ADD CART ========================
-function addToCart(event) {
-  noProd.classList.add('is-hidden');
-   const choosenProduct = event.currentTarget.getAttribute('value');
-
-  // ======================== CHOOSE PRODUCT AND ADD TO LOCALSTORAGE ==================
-   for ( let product of products ) {
-    // ADD PRODUCT TO CART ONLY IF NO REPEAT 
-    if (products.indexOf(product) === choosenProduct - 1 && !choosenProductsForCart.includes(product)) {
-    choosenProductsForCart.push(product); 
-    localStorage.setItem('Prod', JSON.stringify(choosenProductsForCart));
-    listOfProds.insertAdjacentHTML('beforeend', markupProductCard(product));
-    } 
-   };
-   console.log( listOfProds.children);
-};
-
-
-// ==================================================MARKUP CARD ====================================
-function markupProductCard(product) {
-   return  markupP = `<li class="prod-card">
-    <h4 class="prod-card-title">${product.type} "${product.name}"</h4>
-    <div id="counter">
-      <button type="button" data-action="decrement">-1</button>
-      <span id="value">1</span>
-      <button typ="button" data-action="increment">+1</button>
-    </div>
-    <h5>Price: <span>${product.price} UAH</span></h5>
-    <button type="button" class="delete-product" value="${product.id}">Delete</button>
-  </li>`;
-};
-// =====================================================================================================
-
-function counterProd() {
-  const counterValue = document.querySelector('#value');
-  let result = 1;
-
-  const btnIncrement = document.querySelector('[data-action="increment"]');
-  const btnDecrement = document.querySelector('[data-action="decrement"]');
-
-  btnIncrement.addEventListener('click', () => counterValue.textContent = result += 1);
-   btnDecrement.addEventListener('click', () => counterValue.textContent = result -= 1);
+function addCard(event) {
+  addBtnValue = event.currentTarget.getAttribute('value');
+  cardOfProduct.forEach(card => {
+    if(card.getAttribute('value') === addBtnValue) {
+      card.classList.remove('visually-hidden');
+    }
+  });
 };
